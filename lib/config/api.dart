5cb451c14c86462034a19469c4d1006f885462bd
irmanyazid <irmandizay@gmail.com>
2023-02-25 13:37:32 +0700

    insert ipconfig HQ

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,8 +1,11 @@
 class Api {
   // static const baseUrl = 'http://192.168.0.105/api_money_record'; //ipconfig perpustakan
-  // static const baseUrl = 'http:/192.168.72.94/api_money_record'; //ipconfig rumah
+  // static const baseUrl = 'http://192.168.72.94/api_money_record'; //ipconfig rumah
+  // static const baseUrl = 'http://192.168.100.226/api_money_record'; //ipconfig HQ
+  // static const baseurl = 'http://10.224.3.28/api_money_record'; //ipconfig coffee studee
+
   static const baseUrl =
-      'http://10.224.3.28/api_money_record'; //ipconfig coffee studee
+      'http://192.168.100.226/api_money_record'; //ipconfig HQ
 
   static const history = '$baseUrl/history';
   static const user = '$baseUrl/user';
